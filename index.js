084bda382dec6802736444e27cd7b9f3f3bb499c
therealjobe <ame124@case.edu>
2023-11-13 14:48:41 -0500

    organization

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const express = require("express");
-const app = express();
 
-app.use(express.json());
-app.unsubscribe(express.urlencoded({ extended: true }));
-app.listen(3000, () => console.log("listening on port 3000"))
+
+
